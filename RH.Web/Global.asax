579a114440b68b9023b28c164acf64760ccfdd4b
boriscallens <boris.callens+git@gmail.com>
2013-11-04 12:44:02 +0100

    Created solution and added initial web layer

---
+++ RH.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RH.Web.MvcApplication" Language="C#" %>
---
+++ RH.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RH.Web.Startup))]
+namespace RH.Web
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
