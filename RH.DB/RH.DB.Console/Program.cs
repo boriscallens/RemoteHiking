59b2150c52a716ede67aac8c9c90d53640e532d1
boriscallens <boris.callens+git@gmail.com>
2014-01-12 18:13:17 +0100

    FluentMigrate for database migrations
    First steps towards database migration.
    Removed everything else as it's a total mess

---
+++ RH.DB/RH.DB.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using FluentMigrator.Console;
+
+namespace RH.DB.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string assembly = "--a=RH.DB.Migration.dll";
+                string db = "--db=SqlServer";
+                string conn = String.Format("--conn={0}", ConfigurationManager.ConnectionStrings["RemoteHiking"]);
+                string profile = "--profile=Debug";
+                args = new[] { assembly, db, conn, profile };
+
+                new MigratorConsole(args);
+            }
+            catch (ArgumentException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ RH.DB/RH.DB.Migration/Tasks/01_Migration01.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace RH.DB.Migration.Tasks
+{
+    [Migration(01)]
+    public class Migration01: FluentMigrator.Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey();
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("Users");
+            Delete.Table("Users");
+        }
+    }
+}
